44242771dab1dee6ac5b98a7fb4d2246e20f64af
jeong <gitwalker@naver.com>
2022-06-10 10:14:59 +0900

    initialize

---
+++ app.py
@@ -0,0 +1,24 @@
+# app.py
+from flask import Flask, render_template, request
+
+# Flask 객체 인스턴스 생성
+app = Flask(__name__)
+
+
+@app.route('/', methods=('GET','POST'))
+def index():
+    if request.method == "POST":
+        print(request.form.get('user'))
+        user = request.form.get('user')
+        data={'level':60, 'point':360, 'exp':45000}
+        return render_template('index.html', user=user, data=data)
+    elif request.method == "GET":
+        user = "반원"
+        data= {'level':60, 'point':360, 'exp':45000}
+        return render_template('index.html', user=user, data=data)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
